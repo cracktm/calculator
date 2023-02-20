c55911a7ae95b9e52737d942ebec88bd373df24d
ndgde <ivanbujvol5@gmail.com>
2023-02-21 00:20:05 +0300

    expression handler usage example

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include "ParserMathHandler.h"
+#include <vector>
+#include <string>
+
+
+
+
+
+
+
 
 int main() {
 	ParserMathHandler math = ParserMathHandler();
 
-	std::cout << "answer: " << math.operation(1, 1, '+');
+	std::vector<double> nums = {6, 8, 10, 5};
+	std::vector<std::string> signs = {"+", "/", "-"};
+
+	std::cout << "answer: " << math.exprHandler(nums, signs);
 
 	return 0;
 }
