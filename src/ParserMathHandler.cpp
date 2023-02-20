f0baba0eea299d6116f7d5b82a9e6db839ecb035
ndgde <ivanbujvol5@gmail.com>
2023-02-21 00:16:07 +0300

    The data type of numbers has been changed to double. Added: an expression handler and an auxiliary method for it - get sign priority, an operation handler

--- src/ParserMathHandler.cpp
+++ src/ParserMathHandler.cpp
@@ -4,48 +4,36 @@
 
 
 
+double ParserMathHandler::sum(double num1, double num2) { return num1 + num2; }
 
-int ParserMathHandler::sum(int num1, int num2)
-{
-	return num1 + num2;
-}
-
-int ParserMathHandler::sub(int num1, int num2)
-{
-	return num1 - num2;
-}
+double ParserMathHandler::sub(double num1, double num2) { return num1 - num2; }
 
 
 
 
 
-float ParserMathHandler::mul(int num1, int num2)
-{
-	return (float)num1 * (float)num2;
-}
+double ParserMathHandler::mul(double num1, double num2) { return num1 * num2; }
 
-float ParserMathHandler::div(int num1, int num2)
-{
-	return (float)num1 / (float)num2;
-}
+double ParserMathHandler::div(double num1, double num2) { return num1 / num2; }
 
 
 
 
 
-int ParserMathHandler::mod(int num1, int num2)
-{
-	return num1 % num2;
-}
+int ParserMathHandler::mod(int num1, int num2) { return num1 % num2; }
 
-int ParserMathHandler::pow(int num1, int num2)
+
+
+
+
+double ParserMathHandler::pow(double num1, int num2)
 {
 	if (num2 == 2.0) return num1 * num1;
 	if (num2 == -1.0) return 1.0 / num1;
 	if (num2 < 0) return 1.0 / pow(num1, -num2);
 
 
-	float result = 1.0;
+	double result = 1.0;
 	for (int i = 0; i < num2; i++)
 		result *= num1;
 
@@ -64,14 +52,66 @@ int ParserMathHandler::pow(int num1, int num2)
 
 
 
-float ParserMathHandler::operation(int num1, int num2, char sign)
+double ParserMathHandler::opHandler(double num1, double num2, std::string sign)
 {
-	if (sign == '+') return (float) sum(num1, num2);
-	if (sign == '-') return (float)	sub(num1, num2);
-	if (sign == '*') return mul(num1, num2);
-	if (sign == '/') return div(num1, num2);
-	if (sign == '%') return (float) mod(num1, num2);
-	if (sign == '^') return (float) pow(num1, num2);
+	if (sign == "+") return sum(num1, num2);
+	if (sign == "-") return sub(num1, num2);
+	if (sign == "*") return mul(num1, num2);
+	if (sign == "/") return div(num1, num2);
+	if (sign == "%") return (double) mod((int)num1, (int)num2);
+	if (sign == "^") return pow(num1, (int)num2);
 
 	return 0; // default
+}
+
+
+
+
+
+int ParserMathHandler::getSignPriority(std::string sign)
+{
+	if (sign == "+") return 1;
+	if (sign == "-") return 1;
+
+	if (sign == "*") return 2;
+	if (sign == "/") return 2;
+
+	if (sign == "%") return 3;
+	if (sign == "^") return 3;
+
+	return 0; // default
+}
+
+
+
+
+double ParserMathHandler::exprHandler(std::vector<double> nums, std::vector<std::string> signs)
+{
+	if (signs.empty()) return nums.front();
+
+
+	int minPrioritySignIndex = 0;
+	for (int i = 1; i < signs.size(); i++)												//finding of minimum priority sign index
+		if (getSignPriority(signs[i]) < getSignPriority(signs[minPrioritySignIndex]))
+			minPrioritySignIndex = i;
+
+
+
+	return opHandler(
+		exprHandler(
+			std::vector<double>(nums.begin(), nums.begin() + minPrioritySignIndex + 1), 			// left side of numbers	|
+																									// 						| left side of expression
+			std::vector<std::string>(signs.begin(), signs.begin() + minPrioritySignIndex)			// left side of signs	|
+		),
+
+																									// dividing an expression by the minimum priority sign
+
+		exprHandler(
+			std::vector<double>(nums.begin() + minPrioritySignIndex + 1, nums.end()),				// right side of numbers |
+																									// 						 | right side of expression
+			std::vector<std::string>(signs.begin() + minPrioritySignIndex + 1, signs.end())			// right side of signs	 |
+		),
+
+		signs[minPrioritySignIndex] 																// minimum priority sign
+	);
 }
--- src/ParserMathHandler.h
+++ src/ParserMathHandler.h
@@ -1,25 +1,34 @@
 #ifndef __SRC_PARSER_MATH_HANDLER_H_
 #define __SRC_PARSER_MATH_HANDLER_H_
 
+
+#include <vector>
+#include <string>
+
 class ParserMathHandler
 {
 private:
 
-	int sum(int num1, int num2);
-	int sub(int num1, int num2);
-	float mul(int num1, int num2);
-	float div(int num1, int num2);
+	double sum(double num1, double num2);
+	double sub(double num1, double num2);
+
+	double mul(double num1, double num2);
+	double div(double num1, double num2);
+
 	int mod(int num1, int num2);
-	int pow(int num1, int num2);
+	double pow(double num1, int num2);
 
+	
 
+	int getSignPriority(std::string sign);
 
 public:
 	ParserMathHandler() {}
 	~ParserMathHandler() {}
 	
-	float operation(int num1, int num2, char sign);
+	double opHandler(double num1, double num2, std::string sign);
 
+	double exprHandler(std::vector<double> nums, std::vector<std::string> signs);
 };
 
 #endif // __SRC_PARSER_MATH_HANDLER_H_
